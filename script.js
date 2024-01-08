1dc6a958da25e5769e6157be9b7af3d8b5791bce
andre <ank2.qc@gmail.com>
2024-01-08 13:50:08 -0300

    playserselection start

---
+++ script.js
@@ -0,0 +1,17 @@
+const CHOICES = ['rock', 'paper', 'scissor']
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function getComputerChoice(){    
+    return CHOICES[getRandomInt(3);];
+}
+
+
+function playerSelection(){
+
+}
+
+
+console.log(getComputerChoice())
